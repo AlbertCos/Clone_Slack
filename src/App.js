c90b1cb50450d3a3d95a4ea1de22c7aa778e4a05
AlbertCos <cosmenalberto@gmail.com>
2021-03-03 18:41:43 +0100

    ***Brk18:41

--- src/App.js
+++ src/App.js
@@ -1,2 +1,21 @@
-import {ChatEgine} from 'react-chat-engine';
+import {ChatEngine} from 'react-chat-engine';
 import './App.css';
+import ChatFeed from './components/ChatFeed';
+
+const App = () =>{
+  return (
+    <ChatEngine
+      height= "100vh"
+      projectId="f73fca83-dc4c-4f21-8a89-b9c4cf534726"
+      username=""
+      //Password
+      userSecret=""
+      renderChatFeed={(chatAppProps) => <ChatFeed {... chatAppProps }}
+    />
+  );
+}
+
+export default App
+
+//Project ID:f73fca83-dc4c-4f21-8a89-b9c4cf534726
+//Private Key: 52aacd55-7e5b-4db0-8df3-fea49f762f00
---
+++ src/components/ChatFeed.jsx
@@ -0,0 +1,12 @@
+const ChatFeed = (props) =>{
+
+    console.log(props);
+
+    return (
+        <div>
+            ChatFeed
+        </div>
+    );
+}
+
+export default ChatFeed;
