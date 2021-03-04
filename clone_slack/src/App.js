a024731b61da0326a90f39117d2f36896d313dcd
AlbertCos <cosmenalberto@gmail.com>
2021-03-04 23:06:30 +0100

    Coding the Sidebar, styling header

--- clone_slack/src/App.js
+++ clone_slack/src/App.js
@@ -1,17 +1,20 @@
 import React from "react";
 import './App.css';
 import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     //BEM naming convention
     <div className="App">
       <h1>Hello</h1>
-      {/* header*/}
+      
       <Header/>
-      {/*Sidebar*/}
+      <div className="app__body">
+        <Sidebar/>
+     
       {/*react-router -> chat screen */ }
-
+      </div>
     </div>
   );
 }
--- clone_slack/src/Header.js
+++ clone_slack/src/Header.js
@@ -3,9 +3,10 @@ import './Header.css';
 import {Avatar} from "@material-ui/core";
 import AccessTimeIcon from "@material-ui/icons/AccessTime";
 import SearchIcon from "@material-ui/icons/Search";
+import HelpOutlineIcon from "@material-ui/icons/HelpOutline"
 
 function Header () {
-    return <div className = "header">
+    return ( <div className = "header">
             <div className = "header__left">
             <Avatar
                 className="header__avatar"
@@ -16,12 +17,14 @@ function Header () {
             </div>
             <div className = "header__search">
                 <SearchIcon/>
-                {/* input */}
+                <input placeholder= "Search"/>
             </div>
             <div className ="header__right">
-                {/* help icon */}
+            <HelpOutlineIcon/>
+        
             </div>
-        </div>;
-    
+        </div>
+    );
 }
+
 export default Header;
