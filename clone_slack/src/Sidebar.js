c24c51479d5fddaae7e97a450106f3d0a14a369f
AlbertCos <cosmenalberto@gmail.com>
2021-03-04 23:31:48 +0100

    brk 23:31 / 1h** sidebar style

--- clone_slack/src/Sidebar.js
+++ clone_slack/src/Sidebar.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './Sidebar.css'
+import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+import CreateIcon from "@material-ui/icons/Create";
 
 function Sidebar () {
-    return (
+    return ( 
         <div className="sidebar">
             
+            <div className="sidebar__header">
+                <div className="sidebar__info">
+                
+                <h2>Sidebar</h2>
+                <h3>
+                    <FiberManualRecordIcon/>
+                    Albert Cos
+                </h3>
+                </div>
+                <CreateIcon/> 
+            </div>        
         </div>
-    )
+    );
 }
 
 export default Sidebar
